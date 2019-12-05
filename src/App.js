39a8e2eb7813465c20701c86b51d0e55bdab1685
Ben Gallard <bengallard@gmail.com>
2019-12-05 18:17:59 +0000

    lecture 60: nameChangedHandler changes name of event.target.value for the person matching id parameter

--- src/App.js
+++ src/App.js
@@ -20,14 +20,23 @@ class App extends Component {
     this.setState({persons:persons})
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'Ben', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 26 }
-      ]
-    } )
+  nameChangedHandler = ( event, id ) => {
+    const personIndex = this.state.persons.findIndex(person => {
+      return person.id === id
+    })
+
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+    // or...
+    // const person = Object.assign({}, this.state.persons[personIndex])
+
+    person.name = event.target.value
+
+    const persons = [...this.state.persons]
+    persons[personIndex] = person
+
+    this.setState( {persons: persons} )
   }
 
   togglePersonsHandler = () => {
@@ -55,7 +64,8 @@ class App extends Component {
               click={() => this.deletePersonHandler(index)}
               name={person.name}
               age={person.age}
-              key={person.id} />
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)} />
           })}
         </div>
       );
