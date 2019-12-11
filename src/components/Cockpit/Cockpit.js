59feaacf58138b413a1a2fbe4eb7f2cbce31800a
Ben Gallard <bengallard@gmail.com>
2019-12-11 11:16:51 +0000

    lecture 98: Optimize Functional Components with React.memo()

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 
 import classes from './Cockpit.css'
 
-const cockpit = (props) => {
+const cockpit = props => {
     useEffect( () => {
         console.log('[Cockpit.js] useEffect')
         // Http request...
@@ -27,11 +27,11 @@ const cockpit = (props) => {
         btnClass = classes.Red
     }
     
-    if (props.persons.length <= 2) {
+    if (props.personsLength <= 2) {
       assignedClasses.push(classes.red)
     }
 
-    if (props.persons.length <= 1) {
+    if (props.personsLength <= 1) {
       assignedClasses.push(classes.bold)
     }
 
@@ -48,4 +48,4 @@ const cockpit = (props) => {
     )
 }
 
-export default cockpit
+export default React.memo(cockpit)
