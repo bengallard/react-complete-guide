9cdc9059fe67c3814b83116299f257cfe8fe4e4f
Ben Gallard <bengallard@gmail.com>
2019-12-11 14:33:18 +0000

    lecture 102: Use Aux component to render adjacent JSX elements

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+import Aux from '../../../hoc/Aux'
 import classes from './Person.css';
 
 class Person extends Component {
@@ -7,18 +8,17 @@ class Person extends Component {
   render() {
     console.log('[Person.js] rendering...')
     return (
-      <div className={classes.Person}>
-        <p 
-          onClick={this.props.click}>
+    <Aux>
+        <p onClick={this.props.click}>
           I'm {this.props.name} and I'm {this.props.age} years old!
-        </p>
-        <p>{this.props.children}</p>
+        </p>,
+        <p>{this.props.children}</p>,
         <input 
           type="text" 
           onChange={this.props.changed} 
           value={this.props.name} 
         />
-      </div>
+      </Aux>
     )
   }
 
